c336721ed2c398341e77bcf840a14a8659d1862b
AtrebasTheo <AtrebasTS@protonmail.com>
2024-09-23 14:41:29 +0200

    Test 2

--- Java-Game/core/src/main/java/io/github/some_example_name/Main.java
+++ Java-Game/core/src/main/java/io/github/some_example_name/Main.java
@@ -16,7 +16,7 @@ public class Main extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
-        System.out.print("das ist eine Aenderungen");
+        System.out.print("das ist eine Aenderunge");
     }
 
     @Override
